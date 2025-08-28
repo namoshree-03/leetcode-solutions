743203bdd84d7d961689e3412c38519bb43327ae
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2025-08-28 12:38:28 +0530

    Add files via upload

---
+++ FindPivotIndex.java
@@ -0,0 +1,22 @@
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (leftSum == (totalSum - leftSum - nums[i])) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
+
