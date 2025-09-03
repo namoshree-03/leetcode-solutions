ca88bc777b6ffef3ddb1b26fb01c91474249a435
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2025-09-03 20:05:59 +0530

    Add files via upload

---
+++ NumberOfGoodPairs.java
@@ -0,0 +1,11 @@
+public class NumberOfGoodPairs {
+        public int numIdenticalPairs(int[] nums) {
+            int result=0;
+            int count[]=new int[101];
+            for(int i : nums){
+                result+=count[i]++;
+            }
+            return result;
+        }
+    }
+}
