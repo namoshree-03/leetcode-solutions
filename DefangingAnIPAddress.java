1dd67e5f2b818759b43a133f7e1112e0afc81a79
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2025-09-05 16:12:04 +0530

    Add files via upload

---
+++ DefangingAnIPAddress.java
@@ -0,0 +1,14 @@
+public class DefangingAnIPAddress {
+    public String defangIPaddr(String address) {
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i<address.length(); i++){
+            if (address.charAt(i)=='.'){
+                result.append("[.]");
+            }
+            else {
+                result.append(address.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+}
