563cfdc4f6e6fe3499955f178923f64ebfae0de0
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-01-17 23:39:36 +0530

    Add files via upload

---
+++ MaximumProductDifferenceBetweenTwoPairs.java
@@ -0,0 +1,28 @@
+public class MaximumProductDifferenceBetweenTwoPairs {
+    public int maxProductDifference(int[] nums) {
+
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            // find two largest
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+
+            // find two smallest
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        return (max1 * max2) - (min1 * min2);
+    }
+}
