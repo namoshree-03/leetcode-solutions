27fc1238bf298f916710ae771281beb88e5bc978
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-01-18 20:40:16 +0530

    Add files via upload

---
+++ MinimumCostToMoveChipsToSamePosition.java
@@ -0,0 +1,17 @@
+public class MinimumCostToMoveChipsToSamePosition {
+    public int minCostToMoveChips(int[] position) {
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int pos : position) {
+            if (pos % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        return Math.min(evenCount, oddCount);
+    }
+}
