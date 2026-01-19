43e947f2c941425761d18d68f408118be537d921
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-01-19 23:30:16 +0530

    Add files via upload

---
+++ CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,20 @@
+public class CountNegativeNumbersInASortedMatrix {
+    public int countNegatives(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int r = 0;
+        int c = cols - 1;
+        int count = 0;
+
+        while (r < rows && c >= 0) {
+            if (grid[r][c] < 0) {
+                count += (rows - r);
+                c--;
+            } else {
+                r++;
+            }
+        }
+        return count;
+    }
+}
