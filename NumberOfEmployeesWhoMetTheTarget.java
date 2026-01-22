9f6d9236bf194e2fa76d95afc05224c0bd7d4e6d
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-01-22 22:33:47 +0530

    Add files via upload

---
+++ NumberOfEmployeesWhoMetTheTarget.java
@@ -0,0 +1,12 @@
+public class NumberOfEmployeesWhoMetTheTarget {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+        for(int i=0;i<hours.length;i++)
+        {
+            if (hours[i]>=target){
+                count++;
+            }
+        }
+        return count;
+    }
+}
