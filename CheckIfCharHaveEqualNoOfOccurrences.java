5b8f6ce83d7091130fb4a994de25f52d33835a68
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-01-31 23:48:45 +0530

    Add files via upload

---
+++ CheckIfCharHaveEqualNoOfOccurrences.java
@@ -0,0 +1,19 @@
+public class CheckIfCharHaveEqualNoOfOccurrences {
+    public boolean areOccurrencesEqual(String s) {
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+        int expected = 0;
+        for(int f:freq){
+            if (f>0){
+                if(expected==0){
+                    expected = f;
+                } else if (f!=expected){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
