eb5d5781c35cc5c520020a049339d7f7519e73ab
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-05-20 23:27:21 +0530

    Add files via upload

---
+++ BitManipulation/NumberOf1Bits.java
@@ -0,0 +1,11 @@
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((n >> i) & 1) == 1) {
+                res += 1;
+            }
+        }
+        return res;
+    }
+}
