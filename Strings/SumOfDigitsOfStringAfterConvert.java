78c9582521f06a6fe6805f81e36550af82a6ac7d
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-05-31 00:23:29 +0530

    Add files via upload

---
+++ Strings/SumOfDigitsOfStringAfterConvert.java
@@ -0,0 +1,21 @@
+public class SumOfDigitsOfStringAfterConvert {
+    public int getLucky(String s, int k) {
+        int sum = 0;
+        for (char ch : s.toCharArray()){
+            int val = ch - 'a' + 1;
+            while (val>0) {
+                sum += val%10;
+                val/=10;
+            }
+        }
+        for (int i = 1; i < k; i++) {
+            int temp = 0;
+            while (sum > 0) {
+                temp += sum % 10;
+                sum /= 10;
+            }
+            sum = temp;
+        }
+        return sum;
+    }
+}
