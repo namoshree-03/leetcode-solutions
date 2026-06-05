97562d08040d94028d0610b18571065190baccb8
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-06-06 00:05:33 +0530

    Add files via upload

---
+++ Math/HarshadNumber.java
@@ -0,0 +1,14 @@
+public class HarshadNumber {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int original = x;
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        if (original % sum == 0) {
+            return sum;
+        }
+        return -1;
+    }
+}
