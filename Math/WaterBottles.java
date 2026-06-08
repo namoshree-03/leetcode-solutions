57c38f5f7dfcd4d9f8f0fad391c8b28dcee06106
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-06-08 23:23:21 +0530

    Add files via upload

---
+++ Math/WaterBottles.java
@@ -0,0 +1,12 @@
+public class WaterBottles {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles;
+        int empty = numBottles;
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange;
+            total += newBottles;
+            empty = newBottles + (empty % numExchange);
+        }
+        return total;
+    }
+}
