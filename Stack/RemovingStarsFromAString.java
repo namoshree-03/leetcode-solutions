ecb8c9cb59486cd4656f7f3fc870326fee170829
Namoshree Badkhal <namoshreebadkhal03@gmail.com>
2026-07-24 23:54:06 +0530

    Add files via upload

---
+++ Stack/RemovingStarsFromAString.java
@@ -0,0 +1,21 @@
+public class RemovingStarsFromAString {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char ch : stack) {
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
